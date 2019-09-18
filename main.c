82bbb7849383240287634f4006d20a5d3dface73
Kalashnikov Ivan <steindvart.ivan@gmail.com>
2019-09-18 19:37:40 +0700

    correct input size of field

--- main.c
+++ main.c
@@ -21,12 +21,12 @@ int main(void)
 	bool correct_input = false;
 	while (!correct_input)
 	{
-		printw("\nEnter height of field (3, 4, 5): ");
+		printw("\nEnter height of field (3, 4, 5, 6, 7): ");
 		scanw("%d", &size_y);
-		printw("\nEnter width of field (3, 4, 5, 6, 7): ");
+		printw("\nEnter width of field (3, 4, 5): ");
 		scanw("%d", &size_x);
 
-		if ((size_y < 3 || size_y > 5) || (size_x < 3 || size_x > 7))
+		if ((size_y < 3 || size_y > 7) || (size_x < 3 || size_x > 5))
 			printw("\nPlease, enter correct value!");
 		else
 			correct_input = true;
